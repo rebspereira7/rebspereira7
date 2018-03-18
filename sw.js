e52fc67f341198abc2235d2836017520e48fc49a
rebspereira7 <rebspereira7@gmail.com>
2018-03-18 19:37:38 -0300

    script add ao html

--- sw.js
+++ sw.js
@@ -1,16 +1,3 @@
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-        navigator.serviceWorker.register('sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }).catch(function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
-
-
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open('airhorner').then(function(cache) {
