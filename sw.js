3d05e33c3f96a21898e0108f69bae810139a4511
rebspereira7 <rebspereira7@gmail.com>
2018-03-18 18:14:24 -0300

    add cach

--- sw.js
+++ sw.js
@@ -9,3 +9,15 @@ if ('serviceWorker' in navigator) {
         });
     });
 }
+
+
+self.addEventListener('install', function(e) {
+    e.waitUntil(
+        caches.open('airhorner').then(function(cache) {
+            return cache.addAll([
+                '/',
+                '/index.html'
+            ]);
+        })
+    );
+});
