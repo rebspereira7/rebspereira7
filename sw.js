37c98db4aff3cc60be94327fa573cb571bd6a532
rebspereira7 <rebspereira7@gmail.com>
2018-03-18 20:33:05 -0300

    erro typo cache namer

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(e) {
     e.waitUntil(
-        caches.open('airhorner').then(function(cache) {
+        caches.open('Barber').then(function(cache) {
             return cache.addAll([
                 'index.html',
                 'css/style.css'
