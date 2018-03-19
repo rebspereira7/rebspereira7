8e4101c60a7c446d2a1adc1b21066716b617e87b
rebspereira7 <rebspereira7@gmail.com>
2018-03-19 11:35:22 -0300

    add fetch  event

--- sw.js
+++ sw.js
@@ -8,3 +8,22 @@ self.addEventListener('install', function(e) {
         })
     );
 });
+
+self.addEventListener('fetch', function(event) {
+    console.log('Fetch event for ', event.request.url);
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log('Found ', event.request.url, ' in cache');
+                return response;
+            }
+            console.log('Network request for ', event.request.url);
+            return fetch(event.request)
+
+        }).catch(function(error) {
+
+            return caches.match('index.html');
+
+        })
+    );
+});
