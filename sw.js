0fef46422a37e40125705ad474d72338553743aa
rebspereira7 <rebspereira7@gmail.com>
2018-12-11 18:37:21 +0000

    teste de remomocao do sw.js
    para ver se funcional o add to screen

--- sw.js
+++ sw.js
@@ -1,20 +1,2 @@
-self.addEventListener('fetch', function(event) {
-    console.log('Fetch event for ', event.request.url);
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            if (response) {
-                console.log('Found ', event.request.url, ' in cache');
-                return response;
-            }
-            console.log('Network request for ', event.request.url);
-            return fetch(event.request)
-
-        }).catch(function(error) {
-
-            return caches.match('index.html');
-
-        })
-    );
-});
 
 
